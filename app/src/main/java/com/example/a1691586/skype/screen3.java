70fc15f07f8c2da7a91fee3879a0ce442ffa23f8
1691586 <rajbirsinghsaini76@gmail.com>
2017-05-20 13:03:46 -0400

    drawable changes

---
+++ app/src/main/java/com/example/a1691586/skype/screen3.java
@@ -0,0 +1,13 @@
+package com.example.a1691586.skype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class screen3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen3);
+    }
+}
