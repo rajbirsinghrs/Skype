5c044c577cdc595a5acfba10a8bce28800be21ca
TANU SOOD <tanusood125@gmail.com>
2017-05-21 03:18:45 -0400

    tanu........,,.,.,,,........

---
+++ app/src/main/java/com/example/a1691586/skype/newchat.java
@@ -0,0 +1,13 @@
+package com.example.a1691586.skype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newchat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newchat);
+    }
+}
